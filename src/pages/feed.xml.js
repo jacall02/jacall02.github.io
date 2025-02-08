1dcb0992c050199b1083853f8f76ce335e76887e
jacall02 <82378764+jacall02@users.noreply.github.com>
2025-02-08 13:01:03 +0100

    readme

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -5,7 +5,7 @@ export async function GET(context) {
   const blog = await getCollection('blog');
   return rss({
     title: 'Portfolio',
-    description: 'Brutal is a theme for Astro',
+    description: 'Javier Callejo',
     stylesheet: false,
     site: context.site,
     items: blog.map((post) => ({
